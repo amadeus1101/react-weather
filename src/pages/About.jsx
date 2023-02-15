edca8125852e4177a5e08a8fc1a667aa364d3b4a
Artemy-sweden <ghosteagle2020@mail.ru>
2023-02-15 21:50:28 +0300

    About-page-filled

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,4 +1,25 @@
 function About() {
-  return <h1>This page is NOT READY</h1>;
+  return (
+    <article>
+      <h2 className="title">
+        <span className="red">A</span>bout us
+      </h2>
+      <p className="subtitle">Weza &#8594; react-weather-project</p>
+
+      <p>
+        <span className="red">Hi</span> there! I'm Artemy, the creater of Weza.
+        I'm a junior web developer & freelancer & a little bit designer. There
+        should be a link to my <a href="#">github</a> , but for some reason it
+        was eaten by my cat)
+      </p>
+      <p>
+        [Weza] is a pet-project on ReactJS which shows a weather in real-time up
+        to 7 days. Here u can see forecast in ur city and almost in every city u
+        know. Just type the right cityname into the search box and enjoy the
+        result. All data is real and updates every moment.
+      </p>
+      <p>I think, this information will be enough to fill this field;)</p>
+    </article>
+  );
 }
 export default About;
