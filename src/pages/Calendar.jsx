ad7c6723067af7477da3ac55ed41f7ba56040026
Artemy-sweden <ghosteagle2020@mail.ru>
2023-02-18 22:17:34 +0300

    calendar-ready

--- src/pages/Calendar.jsx
+++ src/pages/Calendar.jsx
@@ -1,4 +1,59 @@
 function Calendar() {
-  return <h1>The page is NOT READY</h1>;
+  const date = new Date();
+  const currMonth = date.getMonth();
+  const months = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+  let moons = [
+    8, 8, 8, 8, 1, 2, 2, 2, 2, 2, 2, 2, 3, 4, 4, 4, 4, 4, 4, 5, 6, 6, 6, 6, 6,
+    6, 7, 8,
+  ];
+  let calendar = [];
+
+  if (date.getFullYear() % 4 === 0) months[1] = 29;
+
+  const defineSpecialCases = (i) => {
+    if (i === 0) {
+      return "first";
+    }
+    if (i === date.getDate() - 1) {
+      return "today";
+    } else {
+      return "";
+    }
+  };
+  const fillCalendar = () => {
+    for (let i = 0; i < months[currMonth]; i++) {
+      calendar[i] = {
+        day: i + 1,
+        moon: `${moons[i]}.png`,
+        weekend: (i + 1) % 7 === 4 || (i + 1) % 7 === 5 ? true : false,
+      };
+    }
+  };
+  fillCalendar();
+  return (
+    <>
+      <h2 className="title">
+        <span className="red">M</span>oon Calendar
+      </h2>
+      <p className="subtitle">Meet the new moon-calendar for the month</p>
+      <div className="calendar">
+        <h4>
+          <span className="red">F</span>ebruary-2023
+        </h4>
+        <div className="month-container">
+          {calendar.map((item, index) => (
+            <div
+              id={defineSpecialCases(index)}
+              className={item.weekend ? "month-item weekend" : "month-item"}
+              key={index}
+            >
+              <img src={`../../assets/img/${item.moon}`} alt="moon-phase" />
+              <p className={item.weekend ? "weekend" : ""}>{item.day}</p>
+            </div>
+          ))}
+        </div>
+      </div>
+    </>
+  );
 }
 export default Calendar;
