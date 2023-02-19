633c92f54bf0d77e26ee23041a4126c29b0c3c9d
Artemy-sweden <ghosteagle2020@mail.ru>
2023-02-19 15:07:39 +0300

    add-colorPallete&BurgerMenu&logo

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,28 +1,40 @@
 import { Link } from "react-router-dom";
 import React from "react";
+import { ReactComponent as Logo } from "./logo.svg";
 
 function Header({ changeColorTheme, mode }) {
   const [activeLink, setActiveLink] = React.useState(1);
   const links = ["About us", "Weather", "Moon calendar"];
+  let menu = document.querySelector(".burger");
   const changeActiveLink = (id) => {
     setActiveLink(id);
+  };
+  const onClickBurger = () => {
+    menu.classList.toggle("active-menu");
+    document.querySelector("header").classList.toggle("burger-opened");
+  };
+  const closeMenu = () => {
+    document.querySelector("header").classList.remove("burger-opened");
+    menu.classList.remove("active-menu");
   };
   return (
     <header>
       <nav>
-        <div className="logo">
-          <a href="#">
-            weza <sup>beta</sup>
-          </a>
-        </div>
+        <Link to="/">
+          <div className="logo" onClick={() => closeMenu()}>
+            <Logo />
+            <p>eza</p>
+          </div>
+        </Link>
         <ul>
-          <li>
+          <li className="burger" onClick={() => onClickBurger()}></li>
+          <li onClick={() => closeMenu()}>
             <Link to="/about">About us</Link>
           </li>
-          <li>
+          <li onClick={() => closeMenu()}>
             <Link to="/">Weather</Link>
           </li>
-          <li>
+          <li onClick={() => closeMenu()}>
             <Link to="/calendar">Moon calendar</Link>
           </li>
         </ul>
