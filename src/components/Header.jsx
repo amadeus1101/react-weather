baca8b70d8974e2c9753efb327803c730aa9850b
Artemy-sweden <ghosteagle2020@mail.ru>
2023-02-20 20:11:05 +0300

    header-menu-bug--try

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -21,10 +21,11 @@ function Header({ changeColorTheme, mode }) {
     if (!menuOpened) {
       menu.classList.add("active-menu");
       document.querySelector("header").classList.add("burger-opened");
-      //document.body.style.overflow = "hidden";
+      document.body.style.overflow = "hidden";
     } else {
       menu.classList.remove("active-menu");
       document.querySelector("header").classList.remove("burger-opened");
+      document.body.style.overflow = "auto";
       //document.body.style.overscrollBehavior = "none";
     }
     setMenuOpened(!menuOpened);
