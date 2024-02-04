9fbd9c26d8cf64d083e8a9c341de9fe9870dc1b3
Artemy-sweden <ghosteagle2020@mail.ru>
2024-02-04 17:58:11 +0300

    weza2-sun-fix

--- src/components/SunStat/SunStat.jsx
+++ src/components/SunStat/SunStat.jsx
@@ -1,18 +1,13 @@
 import "./SunStat.scss";
 
 function SunStat({ data }) {
-  console.log(data);
-  //   const sunrise =
-  //     Number(data.sunrise.slice(0, 2)) * 60 + Number(data.sunrise.slice(3));
-  //   const sunset =
-  //     Number(data.sunset.slice(0, 2)) * 60 + Number(data.sunset.slice(3));
   const d = new Date();
   const s_arr = [1, 5, 9, 13, 17, 21];
   return (
     <div className="sun-loop">
       <div className="range">
-        <span>Sunrise: {data.sunrise}</span>
-        <span>Sunset: {data.sunset}</span>
+        <span>Sunrise: {data?.sunrise}</span>
+        <span>Sunset: {data?.sunset}</span>
       </div>
       <input
         type="range"
